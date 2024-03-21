4f723bd5758384a177d9929914a428a10c221183
nmsderp <nmsderp@gmail.com>
2024-03-21 17:30:42 -0400

    the site lol

---
+++ README.md
@@ -0,0 +1,5 @@
+# BismuthiumHost
+Host websites in the cloud for free!
+
+# To Devs:
+I will be hosting this on Glitch, make as many commits to the repo as you want, I will put the changes on Glitch eventually. For testing the website, please clone it and run it.
---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, send_from_directory, request, redirect
+from better_profanity import profanity
+import json
+
+app = Flask(__name__)
+
+with open('templates/bannedip.json', 'r') as banned_ip_file:
+    banned_ips = json.load(banned_ip_file)
+
+@app.route('/<path:filename>')
+def static_file(filename):
+    return send_from_directory('templates', filename)
+
+@app.route('/')
+def home():
+    return send_from_directory('templates', 'index.html')
+
+def save_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+@app.route('/host', methods=['POST'])
+def host_website():
+    website_name = profanity.censor(request.form['WebName'])
+    website_code = profanity.censor(request.form['WebCode'])
+    
+    # Log IP address and website name, for safety
+    ip_address = request.remote_addr
+    log_message = f"{ip_address} has uploaded {website_name}"
+    with open('templates/log/iplog.txt', 'a') as log_file:
+        log_file.write(log_message + '\n')
+    
+    save_file('templates/sites/' + website_name + '.html', website_code)
+    
+    return "Website hosted successfully! View it <a href='/sites/" + website_name + ".html'>here.</a>"
+
+@app.before_request
+def check_banned_ip():
+    # Check if requesting IP is in the list of banned IPs
+    requesting_ip = request.remote_addr
+    if requesting_ip in banned_ips:
+        return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ", code=302)
+
+if __name__ == '__main__':
+    profanity.load_censor_words()
+    app.run()
